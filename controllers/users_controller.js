d7f3f0df7284f948f51f2ae24142c015bdf54063
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-03-18 23:34:38 +0530

    Rendering pages for sign-up and sign-in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,3 +6,17 @@ module.exports.profile = function(req, res){
         title: 'User Profile'
     });
 }
+
+//exporting the sign-up action to render the sign-up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'Codeial | Sign-Up'
+    });
+}
+
+//exporting the sign-in action to render the sign-in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'Codeial | Sign-In'
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,10 @@ const usersController = require('../controllers/users_controller');
 
 //routing the request
 router.get('/profile', usersController.profile);
+//routing the request to sign-up action in users controller
+router.get('/sign-up', usersController.signUp);
+//routing the request to sign-in action in users controller
+router.get('/sign-in', usersController.signIn);
 
 //exporting the router so that it can be used in other files
 module.exports = router;
