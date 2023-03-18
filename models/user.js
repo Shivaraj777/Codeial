b6aa2bf60618fd1842a78612f98747ffc1700b2a
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-03-18 23:11:34 +0530

    Creating the User schema for Codeial

---
+++ models/user.js
@@ -0,0 +1,30 @@
+// Description: thsi file contains the schema for user collection
+
+//require the mongoose module
+const mongoose = require('mongoose');
+
+//create a new schema for user collection
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    }
+}, {
+    //this will automatically add createdAt and updatedAt fields to the schema
+    timestamps: true
+});
+
+//create a model for user collection
+const User = mongoose.model('User', userSchema);
+
+//export the module
+module.exports = User;
