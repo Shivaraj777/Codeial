1d1d1e333c4054f52391f5cf2d9f29bb330a75b0
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-03-25 15:46:29 +0530

    Display posts on webpage and it's related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,35 @@
 // Description: Home controller page contains a list of actions responsible for rendering home page
 
+//import the Post model
+const Post = require('../models/post');
+
 //exporting the home action
 module.exports.home = function(req, res){
     // console.log(req.cookies); //for reading/printing the cookies in console
     // res.cookie('user_id', 25); //used for writing into/modifying the cookies
-    return res.render('home', {
-        title: "Codeial Home"
-    });
+
+    // Post.find({})   //find all the posts in the DB
+    //     .then(posts => {
+    //         return res.render('home', {
+    //             title: "Codeial | Home",
+    //             posts: posts
+    //         });
+    //     })
+    //     .catch(err => {
+    //         console.log(`Error in fetching posts from DB: ${err}`);
+    //         return;
+    //     });
+
+    //find all the post and populate the user of each post
+    Post.find({}).populate('user').exec()   //populate('user') => populate the user of each post
+        .then(posts => {
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts: posts
+            });
+        })
+        .catch(err => {
+            console.log(`Error in fetching posts from DB: ${err}`);
+            return;
+        });
 }
