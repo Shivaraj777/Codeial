10e356ae009e2bfd9b585b4459a72e0589b1fb04
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-04-06 13:56:51 +0530

    Setting up the configuration for using nodemailer to send mails

---
+++ config/nodemailer.js
@@ -0,0 +1,43 @@
+//Description: This file contains the configuration for interacting with the mailtrap service using nodemailer
+
+//importing the nodemailer module
+const nodemailer = require('nodemailer');
+//importing the ejs module
+const ejs = require('ejs');
+
+
+//defining the transporter for sending emails(defines how the communication will take place)
+let transporter = nodemailer.createTransport({
+    service: 'gmail',                //service to be used for sending emails
+    host: 'smtp.gmail.com',         
+    port: 587,
+    secure: false,  
+    auth: {         
+        user: 'codeslayer09876@gmail.com',
+        pass: 'Test@123'
+    }
+});
+
+//rendering the html template for the mail
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;      //variable to store the html template
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),    //path of the html template, relativePath->path where this function is called
+        data,                                                     //data to be passed to the html template
+        function(err, template){
+            if(err){
+                console.log('Error in rendering template');
+                return;
+            }
+            //if no error then store the html template in the mailHTML variable
+            mailHTML = template;
+        }
+    )
+    return mailHTML;
+}
+
+//exporting the transporter and renderTemplate
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
